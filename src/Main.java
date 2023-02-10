5c7e9ee4c104a9f361f3d590865d8b6fde97a78a
shan <shan@MacBook-Air.local>
2023-02-10 18:40:44 +0530

    getSum method created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
+    public static int getSum(int num1,int num2){
+        return num1 + num2;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int sum = getSum(10,20);
+        System.out.println("Sum: "+ sum);
     }
 }
